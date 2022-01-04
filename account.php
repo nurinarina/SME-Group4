534119410a426b8d59ae7e26a4e5e190caa6c310
Farhan Sadiq <farhan_sadiq@live.com>
2022-01-04 13:19:18 +0400

    dbconn changed

--- account.php
+++ account.php
@@ -167,7 +167,7 @@ if (@$_GET['q'] == 'quiz' && @$_GET['step'] == 2 && isset($_SESSION['6e447159425
                         $q = mysqli_query($con, "UPDATE `rank` SET `score`=$sun ,time=NOW() WHERE username= '$username'") or die('Error174');
                     }
                 }
-            header('location:account.php?q=result&eid=' . $_GET[eid]);
+            header('location:account.php?q=result&eid=' . $_GET['eid']);
 }
 
 if (@$_GET['q'] == 'quiz' && @$_GET['step'] == 2 && isset($_GET['start']) && $_GET['start'] == "start" && (!isset($_SESSION['6e447159425d2d']))) {
--- dbConnection.php
+++ dbConnection.php
@@ -1,3 +1,3 @@
 <?php
-	$con= new mysqli('localhost','root','','quiz')or die("Could not connect to mysql".mysqli_error($con));
+	$con= new mysqli('localhost','root','','quiz2')or die("Could not connect to mysql".mysqli_error($con));
 ?>
